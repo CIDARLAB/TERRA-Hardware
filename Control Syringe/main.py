c1a326cf6e6ec1e46288038393a73e34f94853dc
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-06-15 14:27:59 -0400

    working on servo code

--- Control Syringe/main.py
+++ Control Syringe/main.py
@@ -4,35 +4,35 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 channel_list = input('Enter Which Channels are going to be used for servo motors:  \n**Seperate channels with spaces**')
-#channel_list = [int(s) for s in channel_list.split(' ')]
-
-channel_list = channel_list.split(' ')
-
-new_list = []
-
-for s in channel_list:
-    new_list.append(int(s))
-print(new_list)
-print(type(new_list[2]))
-
-
+channel_list = [int(s) for s in channel_list.split(' ')]
 
 
 GPIO.setup(14, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
-p = GPIO.PWM(14, 50)
+p1 = GPIO.PWM(14, 50)
+p2 = GPIO.PWM(4, 50)
 
-p.start(7.5)
+p1.start(7.5)
+p2.start(7.5)
 
 
 try:
     while True:
-        p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+        p1.ChangeDutyCycle(7.5)  # turn towards 90 degree
         time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(2.5)  # turn towards 0 degree
+        p1.ChangeDutyCycle(2.5)  # turn towards 0 degree
         time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(12.5) # turn towards 180 degree
+        p1.ChangeDutyCycle(12.5) # turn towards 180 degree
+        time.sleep(1) # sleep 1 second
+
+        p2.ChangeDutyCycle(7.5)  # turn towards 90 degree
+        time.sleep(1) # sleep 1 second
+        p2.ChangeDutyCycle(2.5)  # turn towards 0 degree
+        time.sleep(1) # sleep 1 second
+        p2.ChangeDutyCycle(12.5) # turn towards 180 degree
         time.sleep(1) # sleep 1 second
 except KeyboardInterrupt:
-    p.stop()
+    p1.stop()
+    p2.stop()
     GPIO.cleanup()
