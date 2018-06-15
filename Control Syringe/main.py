1d3e1da58a57cf46432b8777be6404aefabc4cc3
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-06-15 11:55:34 -0400

    working on servo code

--- Control Syringe/main.py
+++ Control Syringe/main.py
@@ -4,7 +4,10 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 channel_list = input('Enter Which Channels are going to be used for servo motors:  \n**Seperate channels with spaces**')
-channel_list = [int(s) for s in channel_list.split(' ')]
+#channel_list = [int(s) for s in channel_list.split(' ')]
+
+for s in channel_list.split(' ')
+    channel_list[s] = int(s)
 print(channel_list)
 print(type(channel_list[2]))
 
