a6572c11d49c783eb2e51dbe4835c63d37c083a4
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-06-20 10:24:04 -0400

    Begining code for servo motor

--- Control Syringe - Arduino/lib/Adafruit-PWM-Servo-Driver-Library-master/examples/servo/servo.ino
+++ Control Syringe - Arduino/lib/Adafruit-PWM-Servo-Driver-Library-master/examples/servo/servo.ino
@@ -1,20 +1,19 @@
-#include <Arduino.h>
-/***************************************************
+/*************************************************** 
   This is an example for our Adafruit 16-channel PWM & Servo driver
   Servo test - this will drive 8 servos, one after the other on the
   first 8 pins of the PCA9685
 
   Pick one up today in the adafruit shop!
   ------> http://www.adafruit.com/products/815
-
-  These drivers use I2C to communicate, 2 pins are required to
+  
+  These drivers use I2C to communicate, 2 pins are required to  
   interface.
 
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -28,7 +27,7 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // you can also call it with a different address and I2C interface
 //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(&Wire, 0x40);
 
-// Depending on your servo make, the pulse width min and max may vary, you
+// Depending on your servo make, the pulse width min and max may vary, you 
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
@@ -43,12 +42,27 @@ void setup() {
   Serial.println("8 channel Servo test!");
 
   pwm.begin();
-
+  
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 
   delay(10);
 }
 
+// you can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+  
+  pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /= 60;   // 60 Hz
+  Serial.print(pulselength); Serial.println(" us per period"); 
+  pulselength /= 4096;  // 12 bits of resolution
+  Serial.print(pulselength); Serial.println(" us per bit"); 
+  pulse *= 1000000;  // convert to us
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
 
 void loop() {
   // Drive each servo one at a time
@@ -63,4 +77,7 @@ void loop() {
   }
 
   delay(500);
+
+  servonum ++;
+  if (servonum > 7) servonum = 0;
 }
