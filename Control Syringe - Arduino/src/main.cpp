9e90238ed1c679e1c011ce7c8b828a278c70c4d5
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-06-21 01:14:35 -0400

    working on servo

--- Control Syringe - Arduino/src/main.cpp
+++ Control Syringe - Arduino/src/main.cpp
@@ -1,66 +1,74 @@
 #include <Arduino.h>
-/***************************************************
-  This is an example for our Adafruit 16-channel PWM & Servo driver
-  Servo test - this will drive 8 servos, one after the other on the
-  first 8 pins of the PCA9685
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/products/815
-
-  These drivers use I2C to communicate, 2 pins are required to
-  interface.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(&Wire, 0x40);
 
-// Depending on your servo make, the pulse width min and max may vary, you
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+int pin_list;\
+int pin_num;
+int spaces;
 
-// our servo # counter
-uint8_t servonum = 0;
+//Use microseconds to set PWM pulses
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+
+  pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /= 60;   // 60 Hz
+  //Serial.print(pulselength); Serial.println(" us per period");
+  pulselength /= 4096;  // 12 bits of resolution
+  //Serial.print(pulselength); Serial.println(" us per bit");
+  pulse *= 1000000;  // convert to us
+  pulse /= pulselength;
+  //Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+};
+
+//Class to characterize the syringe groups by identifying what channels are within a group and functions to set their states
+class SyringeGroups{
+  public:
+    int pins[1] = {0};
+    void zero(){
+        setServoPulse(pins[0],0.0006);
+      };
+    void ninety(){
+        setServoPulse(pins[0],0.002);
+      };
+    void oneeighty(){
+        setServoPulse(pins[0],0.0015);
+      };
+      };
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("8 channel Servo test!");
-
+  Serial.begin(9600); //set Baud Rate
   pwm.begin();
 
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 
   delay(10);
+
+  //Get user input regarding number of control syringes which channels will be used
+  Serial.print("Enter how many control syringes you will be using for TERRA: \n");
+  //Serial.print("Enter which channels on the Adafruit Motor Shield are going to be used for servo motors:  \n**Seperate channels with spaces**\n");
 }
 
-
 void loop() {
-  // Drive each servo one at a time
-  Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
-  }
 
+  if(Serial.available() > 0){
+    pin_num = Serial.read() - 48;
+    spaces = pin_num - 1;
+    Serial.print(pin_num);
+  };
+
+  while(Serial.available() > pin
+
+  SyringeGroups Group1;
+  Group1.zero();
   delay(500);
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
-  }
-
+  Group1.ninety();
+  delay(500);
+  Group1.oneeighty();
   delay(500);
 }
