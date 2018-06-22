b28cfb49b1757128716baac835d1850860023e58
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-06-22 10:56:27 -0400

    working on servo code

--- Control Syringe - Arduino/src/main.cpp
+++ Control Syringe - Arduino/src/main.cpp
@@ -3,13 +3,12 @@
 #include <Adafruit_PWMServoDriver.h>
 
 
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 #define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
-int pin_list;\
-int pin_num;
-int spaces;
+int group_num;
 
 //Use microseconds to set PWM pulses
 void setServoPulse(uint8_t n, double pulse) {
@@ -29,7 +28,7 @@ void setServoPulse(uint8_t n, double pulse) {
 //Class to characterize the syringe groups by identifying what channels are within a group and functions to set their states
 class SyringeGroups{
   public:
-    int pins[1] = {0};
+    int pins[16];
     void zero(){
         setServoPulse(pins[0],0.0006);
       };
@@ -48,21 +47,57 @@ void setup() {
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 
   delay(10);
-
-  //Get user input regarding number of control syringes which channels will be used
-  Serial.print("Enter how many control syringes you will be using for TERRA: \n");
-  //Serial.print("Enter which channels on the Adafruit Motor Shield are going to be used for servo motors:  \n**Seperate channels with spaces**\n");
 }
-
 void loop() {
 
-  if(Serial.available() > 0){
-    pin_num = Serial.read() - 48;
-    spaces = pin_num - 1;
-    Serial.print(pin_num);
+  // Ask user for number of control syringes they are using
+  Serial.print("Enter how many syringe groups you will be using for TERRA: \n **You can have groups with 1 or more syringes** \n");
+  while(Serial.available() == 0){};
+  group_num = Serial.read() - 48;
+  Serial.print(group_num);
+  Serial.print("\n");
+
+  int k;
+  int j;
+  char pin_list[40];
+  char *pin_pointer[40];
+  String channel_list;
+  SyringeGroups groups[group_num];
+
+  //Give each syringe group object the channel list that it will control
+  if(k == 0){
+    for (k = 0; k < group_num; k++){
+      Serial.print("Enter which channels on the Adafruit Motor Shield are going to be used for Group");
+      Serial.print(k + 1);
+      Serial.print("\n");
+      Serial.print("** Seperate channels using a space **\n");
+      while(Serial.available() == 0) {};
+      channel_list = Serial.readString();
+      channel_list.toCharArray(pin_list,40);
+
+      char *token = strtok(pin_list," ");
+      while(token != NULL){
+        groups[k].pins[j] = int(token);
+        Serial.print(pin_pointer[j]);
+        token = strtok (NULL, " ");
+        j++;
+        };
+      Serial.print("\n");
+    };
   };
 
-  while(Serial.available() > pin
+  Serial.print(groups[1].pins);
+
+  //Create SyringeGroups objects for each channel group
+
+
+
+
+
+
+
+
+
 
   SyringeGroups Group1;
   Group1.zero();
