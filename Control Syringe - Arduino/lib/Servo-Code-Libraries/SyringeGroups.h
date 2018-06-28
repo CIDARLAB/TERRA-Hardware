c2d0ecbaa32013d84622f9f7075483e313732a1f
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-06-28 16:51:15 -0400

    Added folder with libraries for control syringe code

---
+++ Control Syringe - Arduino/lib/Servo-Code-Libraries/SyringeGroups.h
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include "setServoPulse.h"
+
+
+
+//Class to characterize the syringe groups by identifying what channels are within a group and functions to set their states
+class SyringeGroups{
+  public:
+    int pins[16];
+    int pin_num;
+    void off(){
+      for(int i = 0; i < pin_num; i++){
+      setServoPulse(pins[i],0.0008);
+      };
+    };
+    void oneeighty(){
+      setServoPulse(pins[0],0.0022);
+    };
+    void on(){
+      for(int i = 0; i < pin_num; i++){
+        setServoPulse(pins[i],0.0014);
+      };
+    };
+
+    void prnt(){
+      for(int i; i < 16; i++){
+        Serial.println(pins[i]);
+        };
+      };
+};
--- Control Syringe - Arduino/src/main.cpp
+++ Control Syringe - Arduino/src/main.cpp
@@ -1,52 +1,10 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include "SyringeGroups.h"
+#include "setServoPulse.h"
 
-
-
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
-#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
 int group_num;
-
-//Use microseconds to set PWM pulses
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= 60;   // 60 Hz
-  //Serial.print(pulselength); Serial.println(" us per period");
-  pulselength /= 4096;  // 12 bits of resolution
-  //Serial.print(pulselength); Serial.println(" us per bit");
-  pulse *= 1000000;  // convert to us
-  pulse /= pulselength;
-  //Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-};
-
-//Class to characterize the syringe groups by identifying what channels are within a group and functions to set their states
-class SyringeGroups{
-  public:
-    int i;
-    int pins[16];
-    void off(){
-      for (i = 0; i < 16; i++){
-        setServoPulse(pins[i],0.0008);
-      };
-      i = 0;
-    };
-    void oneeighty(){
-      for(i = 0; i < 16; i++){
-        setServoPulse(pins[i],0.0022);
-      };
-      };
-    void on(){
-      for(i = 0; i < 16; i++){
-        setServoPulse(pins[i],0.0014);
-      };
-      };
-};
 
 void setup() {
   Serial.begin(9600); //set Baud Rate
@@ -64,15 +22,13 @@ void loop() {
   group_num = Serial.read() - 48;
   Serial.println(group_num);
 
-  int k;
   int j;
   char pin_list[40];
   String channel_list;
   SyringeGroups groups[group_num];
 
   //Give each syringe group object the channel list that it will control
-  if(k == 0){
-    for (k = 0; k < group_num; k++){
+    for (int k = 0; k < group_num; k++){
       Serial.print("Enter which channels on the Adafruit Motor Shield are going to be used for Group");
       Serial.println(k + 1);
       Serial.println("** Seperate channels using a space **");
@@ -87,9 +43,9 @@ void loop() {
         token = strtok (NULL, " ");
         j++;
         };
+      groups[k].pin_num = j;
       j = 0;
     };
-  };
 
   Serial.println(groups[1].pins[1]);
 
@@ -97,13 +53,23 @@ void loop() {
 
   while(1){
 
-    groups[1].on();
-    delay(5000);
+    delay(10000);
+
+    groups[0].on();
+    delay(1000);
     groups[1].off();
-    delay(5000);
 
+    delay(60000);
 
+    groups[0].off();
+    delay(1000);
+    groups[1].on();
 
+    delay(60000);
+    groups[0].on();
+    delay(1000);
+    groups[1].off();
 
+    delay(60000);
 }
 }
