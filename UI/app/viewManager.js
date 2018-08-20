53e8e6739504bb8ff5306d446619565a38974b5f
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-08-20 11:51:45 -0400

    output tabs with respective tables working

--- UI/app/viewManager.js
+++ UI/app/viewManager.js
@@ -1,5 +1,5 @@
-export default class ViewManager{
-  constructor(){
+export default class ViewManager {
+  constructor() {
     //initialize variables
     let vessel;
     let outputNumber;
@@ -10,56 +10,56 @@ export default class ViewManager{
     let well_96 = [];
     let well_384 = [];
     let counter = 0;
-    let letters = ["A","B","C","D","E","F","G","H"];
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
     well_24[0] = 1
-    for (var i = 1; i < 384 ; i++) {
-      well_24[i] = i+1;
+    for (var i = 1; i < 384; i++) {
+      well_24[i] = i + 1;
     };
 
     well_96[0] = 1;
     for (var i = 1; i < 96; i++) {
-      well_96[i] = i+1;
+      well_96[i] = i + 1;
     };
 
     well_384[0] = 1;
     for (var i = 1; i < 384; i++) {
-      well_384[i] = i+1;
+      well_384[i] = i + 1;
     }
 
     //methods
     function ab2str(buf) {
-        return String.fromCharCode.apply(null, new Uint8Array(buf));
+      return String.fromCharCode.apply(null, new Uint8Array(buf));
     };
 
     function str2ab_newline(str) {
-        str = str + '\n';
-        var buf = new ArrayBuffer(str.length); // 2 bytes for each char
-        var bufView = new Uint8Array(buf);
-        for (var i = 0, strLen = str.length; i < strLen; i++) {
-            bufView[i] = str.charCodeAt(i);
-        };
-        return buf;
+      str = str + '\n';
+      var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+      var bufView = new Uint8Array(buf);
+      for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+      };
+      return buf;
     };
 
     function str2ab(str) {
-        var buf = new ArrayBuffer(str.length); // 2 bytes for each char
-        var bufView = new Uint8Array(buf);
-        for (var i = 0, strLen = str.length; i < strLen; i++) {
-            bufView[i] = str.charCodeAt(i);
-        };
-        return buf;
+      var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+      var bufView = new Uint8Array(buf);
+      for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+      };
+      return buf;
     };
 
-    function checkString(str){
+    function checkString(str) {
       var regex_1 = /\d+\s/;
       let regex_2 = /\d+/;
-      if (str.search(regex_1,str) == 0){
-        str = str.replace(regex_1,"");
-        while (str.search(regex_1,str) == 0) {
-          str = str.replace(regex_1,"");
+      if (str.search(regex_1, str) == 0) {
+        str = str.replace(regex_1, "");
+        while (str.search(regex_1, str) == 0) {
+          str = str.replace(regex_1, "");
         };
-        if (str.search(regex_2,str) == 0) {
+        if (str.search(regex_2, str) == 0) {
           document.getElementById("openSyringe").style.borderColor = "green";
           document.getElementById("openSyringe").style.borderWidth = "0.15rem";
           return true;
@@ -84,141 +84,137 @@ export default class ViewManager{
     this.durationButton = document.getElementById("durationEnter");
 
     //event handlers
-    this.inputButton.addEventListener('click', function (event) {
-      document.getElementById('currentOutput').innerHTML = currentOutput;
+    this.inputButton.addEventListener('click', function(event) {
       vessel = document.getElementById('selectVessel').value;
       outputNumber = document.getElementById('outputNumber').value;
+
+      //create output tabs
+      let nav = "";
+      let nav_content = "";
+      let vessel_ID = "";
+      let thead_ID = "";
+      let tbody_ID = "";
+      let thead_array = [];
+      let tbody_array = [];
+      for (let k = 0; k < outputNumber; k++) {
+        let output_insert = "Output" + (k + 1);
+        let navID = output_insert + "tab";
+        let href_insert = "#" + output_insert;
+
+        vessel_ID = "vessel" + k;
+        thead_ID = "thead" + k;
+        tbody_ID = "tbody" + k;
+
+        thead_array.push(thead_ID)
+        tbody_array.push(tbody_ID);
+        nav += "<a class='nav-item nav-link' id='" + navID + "' data-toggle='tab' href='" + href_insert + "' role='tab' aria-controls='" + output_insert + "' aria-selected='false'>" + (k + 1) + "</a>";
+        nav_content += "<div class='tab-pane fade' id='" + output_insert + "' role='tabpanel' aria-labelledby='" + navID + "'><table class='table table-bordered table-sm' id='" + vessel_ID + "'><thead id='" + thead_ID + "'></thead><tbody id='" + tbody_ID + "'></tbody></table></div>";
+      };
+
+      console.log(tbody_array[0]);
+      document.getElementById('navTabs').innerHTML = nav;
+      document.getElementById('navContent').innerHTML = nav_content;
 
       //create table for output vessel image
       //create 24-well plate
       if (vessel == 24) {
-        let thead_insert = "<th scope='col'> </th>";
-        for (var i = 1; i < 7; i++){
-          thead_insert += "<th scope='col'>"+i+"</th>";
-        };
-        thead_insert += "</tr>";
+        for (var k = 0; k < outputNumber; k++) {
+          let thead_insert = "<th scope='col'> </th>";
+          for (var i = 1; i < 7; i++) {
+            thead_insert += "<th scope='col'>" + i + "</th>";
+          };
+          thead_insert += "</tr>";
 
-        let tbody_insert = "";
-        let letters = ["A","B","C","D"];
-        for (var i = 0;i < 4; i++){
-          tbody_insert += "<tr>";
-          tbody_insert += "<th scope='row'>"+letters[i]+"</th>";
-            for (var j = 1; j < 7; j++) {
-              let coordinate = letters[i]+j;
-              tbody_insert += "<td><button type='button' class='btn btn-primary btn-sm' onclick='appendXY(this)' value='"+j+"' id='"+coordinate+"'>"+coordinate+"</button></td>";
+          let tbody_insert = "";
+          let letters = ["A", "B", "C", "D"];
+          for (let i = 0; i < 4; i++) {
+            tbody_insert += "<tr>";
+            tbody_insert += "<th scope='row'>" + letters[i] + "</th>";
+            for (let j = 1; j < 7; j++) {
+              let coordinate = letters[i] + j;
+              tbody_insert += "<td><button type='button' class='btn btn-primary btn-sm' onclick='appendXY(this)' value='" + j + "' id='" + coordinate + "'>" + coordinate + "</button></td>";
             };
-          tbody_insert += "</tr>";
+            tbody_insert += "</tr>";
+          };
+
+          document.getElementById(tbody_array[k]).innerHTML = tbody_insert;
+          document.getElementById(thead_array[k]).innerHTML = thead_insert;
         };
-        document.getElementById('tbody_insert').innerHTML = tbody_insert;
-        document.getElementById('thead_insert').innerHTML = thead_insert;
         counter = 0;
-        for (var i = 0; i < 4; i++) {
-          for(var j = 1; j < 7; j++){
-            let identifier = letters[i]+j;
+        for (let i = 0; i < 4; i++) {
+          for (let j = 1; j < 7; j++) {
+            let identifier = letters[i] + j;
             document.getElementById(identifier).value = well_24[counter];
             counter = counter + 1;
           }
         }
       };
 
+
       //create 96-well plate
       if (vessel == 96) {
-        let thead_insert = "<th scope='col'> </th>";
-        for (var i = 1; i < 13; i++){
-          thead_insert += "<th scope='col'>"+i+"</th>";
-        };
-        thead_insert += "</tr>";
+        for (var k = 0; k < outputNumber; k++) {
+          let thead_insert = "<th scope='col'> </th>";
+          for (var i = 1; i < 13; i++) {
+            thead_insert += "<th scope='col'>" + i + "</th>";
+          };
+          thead_insert += "</tr>";
 
-        let tbody_insert = "";
-        let letters = ["A","B","C","D","E","F","G","H"];
+          let tbody_insert = "";
+          let letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
-        for (var i = 0; i < 8; i++){
-          tbody_insert += "<tr>";
-          tbody_insert += "<th scope='row'>"+letters[i]+"</th>";
+          for (var i = 0; i < 8; i++) {
+            tbody_insert += "<tr>";
+            tbody_insert += "<th scope='row'>" + letters[i] + "</th>";
             for (var j = 1; j < 13; j++) {
-              let coordinate = letters[i]+j;
-              tbody_insert += "<td><button type='button' class='btn btn-primary btn-sm' onclick='appendXY(this)' value='"+coordinate+"' id='"+coordinate+"'>"+coordinate+"</button></td>";
+              let coordinate = letters[i] + j;
+              tbody_insert += "<td><button type='button' class='btn btn-primary btn-sm' onclick='appendXY(this)' value='" + coordinate + "' id='" + coordinate + "'>" + coordinate + "</button></td>";
             };
-          tbody_insert += "</tr>";
-        };
-        document.getElementById('tbody_insert').innerHTML = tbody_insert;
-        document.getElementById('thead_insert').innerHTML = thead_insert;
-        counter = 0;
-        for (var i = 0; i < 7; i++) {
-          for(var j = 1; j < 13; j++){
-            let identifier = letters[i]+j;
-            document.getElementById(identifier).value = well_96[counter];
-            counter = counter + 1;
+            tbody_insert += "</tr>";
+          };
+
+          document.getElementById(tbody_array[k]).innerHTML = tbody_insert;
+          document.getElementById(thead_array[k]).innerHTML = thead_insert;
+          counter = 0;
+          for (var i = 0; i < 7; i++) {
+            for (var j = 1; j < 13; j++) {
+              let identifier = letters[i] + j;
+              document.getElementById(identifier).value = well_96[counter];
+              counter = counter + 1;
+            }
           }
-        }
+        };
       };
 
       //create 384-well plate
       if (vessel == 384) {
-        let thead_insert = "<th scope='col'> </th>";
-        for (var i = 1; i < 25; i++){
-          thead_insert += "<th scope='col'>"+i+"</th>";
-        };
-        thead_insert += "</tr>";
+        for (var k = 0; k < outputNumber; k++) {
+          let thead_insert = "<th scope='col'> </th>";
+          for (var i = 1; i < 25; i++) {
+            thead_insert += "<th scope='col'>" + i + "</th>";
+          };
+          thead_insert += "</tr>";
 
-        let tbody_insert = "";
-        let letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"];
-        for (var i = 0;i < 15; i++){
-          tbody_insert += "<tr>";
-          tbody_insert += "<th scope='row'>"+letters[i]+"</th>";
+          let tbody_insert = "";
+          let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"];
+          for (var i = 0; i < 15; i++) {
+            tbody_insert += "<tr>";
+            tbody_insert += "<th scope='row'>" + letters[i] + "</th>";
             for (var j = 1; j < 25; j++) {
-              let coordinate = letters[i]+j;
-              tbody_insert += "<td><button type='button' class='btn btn-primary btn-sm' onclick='appendXY(this)' value='"+coordinate+"' id='"+coordinate+"'>"+coordinate+"</button></td>";
+              let coordinate = letters[i] + j;
+              tbody_insert += "<td><button type='button' class='btn btn-primary btn-sm' onclick='appendXY(this)' value='" + coordinate + "' id='" + coordinate + "'>" + coordinate + "</button></td>";
             };
-          tbody_insert += "</tr>";
+            tbody_insert += "</tr>";
+          };
+          document.getElementById(tbody_array[k]).innerHTML = tbody_insert;
+          document.getElementById(thead_array[k]).innerHTML = thead_insert;
         };
-        document.getElementById('tbody_insert').innerHTML = tbody_insert;
-        document.getElementById('thead_insert').innerHTML = thead_insert;
       };
+
       socket.emit("send-raw", {
-          "name": '/dev/cu.usbmodem1411',
-          "payload": str2ab_newline(outputNumber)
+        "name": '/dev/cu.usbmodem1411',
+        "payload": str2ab_newline(outputNumber)
       })
-    });
-
-    this.nextButton.addEventListener('click',function(event){
-      if (currentOutput < outputNumber) {
-        currentOutput = currentOutput + 1;
-        document.getElementById('currentOutput').innerHTML = currentOutput;
-      } else if (currentOutput == (outputNumber - 1)) {
-        currentOutput = outputNumber;
-        document.getElementById('currentOutput').innerHTML = currentOutput;
-      };
-      console.log(xy);
-      socket.emit("send-raw", {
-          "name": '/dev/cu.usbmodem1411',
-          "payload": str2ab(xy)
-      })
-    });
-
-    this.openButton.addEventListener('click', function (event) {
-      let data = document.getElementById('openSyringe').value;
-      if (checkString(data)) {
-        socket.emit("send-raw", {
-            "name": '/dev/cu.usbmodem1411',
-            "payload": str2ab(data)
-        })
-      } else {
-        console.log("Try again");
-      }
-    });
-
-    this.closeButton.addEventListener('click',function (event){
-      let data = document.getElementById('closeSyringe').value;
-      checkString(data);
-      if (checkString(data)) {
-        socket.emit("send-raw", {
-            "name": '/dev/cu.usbmodem1411',
-            "payload": str2ab(data)
-        });
-      } else {
-        console.log("Try again");
-      }
     });
   };
 };
