bc796f3c6ae21d72a61ce2c675104cd056d7f5c2
Ezira Yimer Wolle <ezirayw@bu.edu>
2018-10-11 20:58:55 -0400

    hello

--- xyPlane/XYplane_August/XYplane_August.ino
+++ xyPlane/XYplane_August/XYplane_August.ino
@@ -7,12 +7,15 @@
 
 
 // Important Definitions
-#define pinEnable    4  // PWM or Enable pins
-#define pinStep      3  // Step 3
-#define pinDir       2  // Direction 2
-#define pinEnable_2  13 // PWM or enable pins
-#define pinStep_2    9  // Step 92
-#define pinDir_2     8  // Direction  8
+#define pinEnable    13  // PWM or Enable pins
+#define pinStep      9  // Step 3
+#define pinDir       8  // Direction 2
+#define pinEnable_2  4 // PWM or enable pins
+#define pinStep_2    3  // Step 92
+#define pinDir_2     2  // Direction  8
+#define limSwitch_1  7  // first Limit Switch
+#define limSwitch_2  6  // second Limit Switch
+
 
 //~~~~~~~~~~ Microstepping Features ~~~~~~~~~~//
 #define microStep1  5   // mode select for microstepping
@@ -22,11 +25,17 @@
 //Syringe Variables
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 char incomingData = 0;
+char incomingData_r = 0;
 int outputNum = 0;
+int reset = 0;
+int dispense_time = 0;
+int flush_time = 0;
+int disp_flush[2] = {0,0};
+int disp_flush_counter = 0;
 
 
 void setup(){
-  Serial.begin(9600); // set baud rate for communication
+  Serial.begin(1152000); // set baud rate for communication
   //Ezira code
   pwm.begin();
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
@@ -52,51 +61,87 @@ void setup(){
   digitalWrite (microStep1, HIGH);
   digitalWrite (microStep2, HIGH);
 
+  // Limit Switch initialization
+
+  digitalWrite (limSwitch_1, HIGH);
+
 
 }
 
 void loop(){
   //  Ezira code
   //  Ask user for number of outputs their microfluidic chip has
-  Serial.print("Enter how many outputs your microfluidic chip contains:");
   while(Serial.available() == 0){};
 
   if (Serial.available() > 0) {
     incomingData = 0;
     while(1) {
       incomingData = Serial.read();
-      if (incomingData == '\n') break;
-      if (incomingData == -1) continue;
-      outputNum *= 10;
-      outputNum = ((incomingData - 48) + outputNum);
-    }
-  }
-  Serial.println(outputNum);
+
+      if (incomingData == 'r'){
+        reset = 1;
+        while(Serial.available() == 0){};
+        if (Serial.available() > 0) {
+          incomingData_r = 0;
+          while(1) {
+            incomingData_r = Serial.read();
+            if (incomingData_r == '\n') break;
+            if (incomingData_r == -1) continue;
+            outputNum *= 10;
+            outputNum = ((incomingData_r - 48) + outputNum);
+          };
+        };
+      } else {
+          if (incomingData == '\n') break;
+          if (incomingData == -1) continue;
+          outputNum *= 10;
+          outputNum = ((incomingData - 48) + outputNum);
+      };
+    };
+  };
 
   Outputs outputs[outputNum];
-
   const int openPins[] = {0,2,4,6,8,10,12,14};
   const int closePins[] = {1,3,5,7,9,11,13,15};
+
 //  Establish pins sequence of open and closed valves for each output
   for (int k = 0; k < outputNum; k++){
     outputs[k].assign_open(openPins[k]);
     outputs[k].assign_close(closePins[k]);
+    if (reset == 1){
+      outputs[k].origin();
+      };
     outputs[k].assign_coordinates();
   };
 
-  Serial.println("Pins");
-  Serial.print(outputs[0].Open.pins[0]);
-  Serial.print(outputs[1].Close.pins[0]);
+  for (int k = 0; k < outputNum; k++){
+    outputs[k].close();
+  };
+
+  while(Serial.available() == 0){};
+  if (Serial.available() > 0) {
+    String dispenseFR;
+    char dispenseFR_list[100];
+    while(Serial.available() == 0){};
+    dispenseFR = Serial.readString();
+    dispenseFR.toCharArray(dispenseFR_list, 100);
+
+    char *token = strtok (dispenseFR_list," ");
+    while (token != NULL) {
+      disp_flush[disp_flush_counter] = atoi(token);
+      disp_flush_counter++;
+      token = strtok (NULL, " ");
+    };
+  };
+
+  dispense_time = disp_flush[0];
+  Serial.print(dispense_time);
+  flush_time = disp_flush[1];
 
 
 
 
-  //Control the actuation of control syringe groups
 
- /* while(1){
-    outputs[0].origin();
-  }*/
-  //Ezira code
 
 
 
@@ -142,15 +187,19 @@ for (int outputIterator = 0; outputIterator < outputNum; outputIterator++){
   digitalWrite( pinStep  , LOW);  // initialize motor 2 to be not moving
 
   for (ivy=0; ivy<(800*4); ivy++){
-  Serial.println(ivy);
-  digitalWrite( pinStep, HIGH);
-  digitalWrite( pinStep_2, HIGH);
-  delay(1);
-  digitalWrite(pinStep, LOW);
-  digitalWrite(pinStep_2, LOW);
-  delay(1);
-  }
+      if ( digitalRead(7) == LOW){
+        break;
+      }
+          Serial.println(ivy);
+          digitalWrite( pinStep, HIGH);
+          digitalWrite( pinStep_2, HIGH);
+          delay(1);
+          digitalWrite(pinStep, LOW);
+          digitalWrite(pinStep_2, LOW);
+          delay(1);
+  };
 
+  delay(flush_time*1000);
 
 
 int Xbefore = 0;
@@ -205,14 +254,14 @@ for (int size = 1; size < (outputs[outputIterator].coordinates.size() + 1); size
              digitalWrite(pinDir_2,LOW);
            }
 
-            for (i = 0; i < ((45*4)*Xnow); i++){
+            for (i = 0; i < ((48.5*4)*Xnow); i++){
               digitalWrite (pinStep, HIGH);
               delay(10);
               digitalWrite (pinStep,LOW);
               delay(10);
             }
 
-            for (i = 0; i < ((45*4)*Ynow); i++){
+            for (i = 0; i < ((48.5*4)*Ynow); i++){
               digitalWrite (pinStep_2, HIGH);
               delay(10);
               digitalWrite (pinStep_2,LOW);
@@ -231,7 +280,7 @@ for (int size = 1; size < (outputs[outputIterator].coordinates.size() + 1); size
             // open syringes - output is released
             outputs[outputIterator].open();
 
-            delay(10000); // dispense time (can be dictated by flowrate)
+            delay(dispense_time*1000); // dispense time (can be dictated by flowrate)
 
             // close valves, all fluids go to waste
             outputs[outputIterator].close();
@@ -251,16 +300,3 @@ for (int size = 1; size < (outputs[outputIterator].coordinates.size() + 1); size
 while (true);
 
 }
-/*
-void write_vector(const std::vector<int>& V){
-   Serial.println ("The well plate locations are: ");
-  for(int i=0; i < V.size(); i++)
-    Serial.print (V[i]);
-    Serial.println (" ");
-    Serial.println (V.size());
-}
-
-bool comp(const int& num1, const int& num2) {
-    return num1 < num2;
-}
-*/
